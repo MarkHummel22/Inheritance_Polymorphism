8be735d5cd17c0a339012b6f8fde74d936056d54
Mark <hummel.marcus@icloud.com>
2022-11-25 23:42:08 -0500

    Here I set up my files and the basic outline of the program. More of a skeleton really. Had to check stackoverflow to figure out how to get the string class to work in the header file for the manufacturer variable

---
+++ Inheritance_Polymorphism/Vehicle.cpp
@@ -0,0 +1,17 @@
+// Mark Hummel 
+// CIS 1202 Module 14
+// 11/25/2022
+
+
+#include "Vehicle.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+
+
+	return 0;
+}
---
+++ Inheritance_Polymorphism/Vehicle.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <string>
+#ifndef VEHICLE_H
+#define VEHICLE_H
+
+
+class Vehicle
+{
+protected:
+	std::string manufacturer;
+
+
+
+
+};
+
+
+
+
+#endif // !VEHICLE_H
