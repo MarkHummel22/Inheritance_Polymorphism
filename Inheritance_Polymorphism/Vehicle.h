ac50b0eff9f2b24f090d26c24c45c5940b13fe4e
Mark <hummel.marcus@icloud.com>
2022-11-25 23:55:22 -0500

    Filled out the Vehicle.h file, looks pretty good to me

--- Inheritance_Polymorphism/Vehicle.h
+++ Inheritance_Polymorphism/Vehicle.h
@@ -7,14 +7,34 @@
 class Vehicle
 {
 protected:
-	std::string manufacturer;
-
-
-
+	string manufacturerName; // for holding the manufaturer name
+	int yearBuilt; // for holding the year the vehicle was built
+public:
+	// Default constructor
+	Vehicle() {
+		manufacturerName = "NONAMEGIVEN";
+		yearBuilt = 1900;
+	}
+	// Constructor
+	Vehicle(string mN, int yr) {
+		manufacturerName = mN;
+		yearBuilt = yr;
+	}
+	// getters and setters
+	string getManufacturerName() const{
+		return manufacturerName;
+	}
+	void setManufacturerName(string newMN) {
+		manufacturerName = newMN;
+	}
+	int getYearBuilt() const {
+		return yearBuilt;
+	}
+	void setYearBuilt(int newYr) {
+		yearBuilt = newYr;
+	}
+	void displayInfo();
 
 };
-
-
-
 
 #endif // !VEHICLE_H
